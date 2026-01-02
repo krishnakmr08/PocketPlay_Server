5e34f68ca2f02ea5b1dfd03a83f9615493a1073a
Krishna Kumar <kkbhagat20@gmail.com>
2026-01-02 12:40:11 +0530

    add Google email verification in sign-in flow

---
+++ src/controllers/auth.js
@@ -0,0 +1,115 @@
+const { OAuth2Client } = require("google-auth-library");
+const BadRequestError = require("../errors/bad-request");
+const UnauthenticatedError = require("../errors/unauthenticated");
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+
+const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const generateUniqueUsername = async (name) => {
+  let username;
+  let isUnique = false;
+
+  while (!isUnique) {
+    username =
+      name.replace(/\s/g, "").toLowerCase().substring(0, 6) +
+      Math.random().toString(36).substr(2, 6);
+
+    const existingUser = await User.findOne({ username });
+    if (!existingUser) {
+      isUnique = true;
+    }
+  }
+
+  return username;
+};
+
+const signInWithGoogle = async (req, res) => {
+  const { id_token } = req.body;
+
+  if (!id_token) {
+    throw new BadRequestError("ID token required");
+  }
+
+  try {
+    const ticket = await googleClient.verifyIdToken({
+      idToken: id_token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+
+    const { email, email_verified } = payload;
+
+    if (!email || !email_verified) {
+      throw new UnauthenticatedError("Google authentication failed");
+    }
+
+    let user = await User.findOne({ email: email });
+
+    if (user) {
+      const accessToken = user.createAccessToken();
+      const refreshToken = user.createRefreshToken();
+
+      return res.status(StatusCodes.OK).json({
+        user,
+        tokens: { access_token: accessToken, refresh_token: refreshToken },
+      });
+    }
+
+    const username = await generateUniqueUsername(payload.name);
+
+    user = new User({
+      email: payload.email,
+      username: username,
+      name: payload.name,
+      userImage: payload.picture,
+    });
+
+    await user.save();
+
+    const accessToken = user.createAccessToken();
+    const refreshToken = user.createRefreshToken();
+
+    res.status(StatusCodes.CREATED).json({
+      user,
+      tokens: { access_token: accessToken, refresh_token: refreshToken },
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const refreshToken = async (req, res) => {
+  const { refresh_token } = req.body;
+  if (!refresh_token) {
+    throw new BadRequestError("Refresh token is required");
+  }
+
+  try {
+    const payload = jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET);
+    const user = await User.findById(payload.id);
+
+    if (!user) {
+      throw new UnauthenticatedError("Invalid refresh token");
+    }
+
+    const newAccessToken = user.createAccessToken();
+    const newRefreshToken = user.createRefreshToken();
+
+    res.status(StatusCodes.OK).json({
+      access_token: newAccessToken,
+      refresh_token: newRefreshToken,
+    });
+  } catch (error) {
+    console.error(error);
+    throw new UnauthenticatedError("Invalid refresh token");
+  }
+};
+
+module.exports = {
+  signInWithGoogle,
+  refreshToken,
+};
